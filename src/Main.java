506973c3afa64ded8d6338f2dd0dfab413c4f0dd
Carter <carterleebainter@gmail.com>
2023-09-22 16:47:06 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+     Scanner in = new Scanner(System.in);
+     int productPrice = 0;
+     double shippingCost = 0;
+     double finalCost = 0;
+
+        System.out.println("Enter the cost of your product: ");
+        productPrice = in.nextLine();
+
+        if (productPrice <= 100)
+    }
+}
